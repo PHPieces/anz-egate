5e374d67b2511b38f38553c18c20b2529b28f6c3
Joseph Lyons <jlyons@bglcorp.com.au>
2017-02-13 15:47:56 +1100

    removed unused code

--- src/Config.php
+++ src/Config.php
@@ -7,22 +7,6 @@ class Config
     private static $baseDir = __DIR__."/../config/";
     private static $store = [];
 
-//    public static function setBaseDir($path)
-//    {
-//        self::$baseDir = $path;
-//    }
-//
-//    public static function storeAll($config = [])
-//    {
-//        if (count($config) > 0) {
-//            self::$store = array_merge(self::$store, $config);
-//            return;
-//        }
-//        foreach (glob(self::$baseDir."*.php") as $file) {
-//            self::loadToStore(basename($file, ".php"));
-//        }
-//    }
-
     public static function load($path)
     {
         if (!self::loaded($path)) {
